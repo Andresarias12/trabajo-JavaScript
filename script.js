41efc29fa3333edc4da5c79ff74fbf31406e08b2
andresfeli <andresarihenao12@gmail.com>
2024-05-06 15:56:05 -0300

    Entrega numero de 3 de proyect

--- script.js
+++ script.js
@@ -1,213 +1,167 @@
-/*alert("Bienvenido/a al parque de la costa!")
-
-function Inicio(nom){
-     return nom.toUpperCase()
-     
-}
-let nombre = prompt ("ingrese su nombre")
-let resultado = Inicio(nombre)
-alert("Bienvenido/a" + " " + resultado)
-
-function menuIncio() {
-        let opcion
-        let total = 0
-        do {
-            opcion = Number(prompt("Ingrese un numero \n1 menores de edad: valor 10$ \n2 mayores de edad: 20$ \n3 Edad avanzada: 5$ \n0 para salir "))
-    
-            if(opcion === 1) {
-               total= agregadoAlCarrito("menor de Edad", 10, total)
-            }  else if(opcion === 2) {
-              total= agregadoAlCarrito("mayor de edad", 20, total)
-            } else if(opcion === 3) {
-              total= agregadoAlCarrito("edad avanzada", 5, total)
-            }
-        
-        
-        } while (opcion !== 0) 
-        alert(" el total de su compra es " + total)
-
-}
-    
-    
-function agregadoAlCarrito(opcion, valor,total) {
-    let cantidad = Number(prompt("Cuantas entradas quieres"))
-    let subtotal = valor*cantidad 
-    total= total + subtotal 
-    alert("se agrego al carrito" + " " + cantidad + " entradas " +  " su valor es de " + " " + subtotal) 
-    if (cantidad >=1 ) {
-      let nombre = prompt ("ingrese nombre de la persona/s")
-      
-    }
-    return total
-}
-menuIncio()*/
-
 let productos = [
-  {
-    id: 5,
-    nombre: "pelotda de futbol",
-    categoria: "deportes",
-    stock: 2,
-    precio: 1000,
-  },
-  {
-    id: 4,
-    nombre: "gorra1",
-    categoria: "indumentaria",
-    stock: 1,
-    precio: 1500,
-  },
-  {
-    id: 6,
-    nombre: "camisa",
-    categoria: "indumentaria",
-    stock: 7,
-    precio: 3250,
-  },
-  {
-    id: 8,
-    nombre: "pantalon",
-    categoria: "indumentaria",
-    stock: 9,
-    precio: 2500,
-  },
-  { id: 9, nombre: "buzo", categoria: "indumentaria", stock: 3, precio: 4000 },
-  {
-    id: 12,
-    nombre: "short de basquet",
-    categoria: "deportes",
-    stock: 5,
-    precio: 3000,
-  },
-  {
-    id: 14,
-    nombre: "short de basquet2",
-    categoria: "deportes",
-    stock: 5,
-    precio: 3000,
-  },
-  {
-    id: 2,
-    nombre: "camisa manga larga",
-    categoria: "indumentaria",
-    stock: 5,
-    precio: 4500,
-  },
-  {
-    id: 15,
-    nombre: "musculosa nike",
-    categoria: "deportes",
-    stock: 2,
-    precio: 2000,
-  },
-  {
-    id: 16,
-    nombre: "chaqueta yankee",
-    categoria: "indumentaria",
-    stock: 6,
-    precio: 3200,
-  },
-  {
-    id: 18,
-    nombre: "conjunto lacoste",
-    categoria: "indumentaria",
-    stock: 10,
-    precio: 6000,
-  },
-  {
-    id: 19,
-    nombre: "camisa de boca",
-    categoria: "deportes",
-    stock: 10,
-    precio: 3500,
-  },
-  {
-    id: 20,
-    nombre: "camisa de river",
-    categoria: "deportes",
-    stock: 10,
-    precio: 3500,
-  },
-  {
-    id: 21,
-    nombre: "camisa de defensa y justicia",
-    categoria: "deportes",
-    stock: 10,
-    precio: 3500,
-  },
-];
+  { id: 5, nombre: "pelotda de futbol", categoria: "deportes", stock: 2, precio: 1000, rutaimagen: "pelota-de-futbol.jpeg" },
+  { id: 4, nombre: "gorra1", categoria: "indumentaria", stock: 1, precio: 1500, rutaimagen: "gorra1.jpeg" },
+  { id: 6, nombre: "camisa", categoria: "indumentaria", stock: 7, precio: 3250, rutaimagen: "camisa.jpeg" },
+  { id: 8, nombre: "pantalon", categoria: "indumentaria", stock: 9, precio: 2500, rutaimagen: "pantalon.jpeg" },
+  { id: 9, nombre: "buzo", categoria: "indumentaria", stock: 3, precio: 4000, rutaimagen: "buzo.jpeg" },
+  { id: 12, nombre: "short de basquet", categoria: "deportes", stock: 5, precio: 3000, rutaimagen: "short-de-basquet.jpeg" },
+  { id: 14, nombre: "short de basquet2", categoria: "deportes", stock: 5, precio: 3000, rutaimagen: "short-de-basquet-2.jpeg" },
+  { id: 2, nombre: "camisa manga larga", categoria: "indumentaria", stock: 5, precio: 4500, rutaimagen: "camisa-manga-larga.jpeg" },
+  { id: 15, nombre: "musculosa nike", categoria: "deportes", stock: 2, precio: 2000, rutaimagen: "musculosa-nike.jpeg" },
+  { id: 16, nombre: "chaqueta yankee", categoria: "indumentaria", stock: 6, precio: 3200, rutaimagen: "chaqueta-yankee.jpeg" },
+  { id: 18, nombre: "conjunto lacoste", categoria: "indumentaria", stock: 10, precio: 6000, rutaimagen: "conjunto-lacoste.jpeg" },
+  { id: 19, nombre: "camisa de boca", categoria: "deportes", stock: 10, precio: 3500, rutaimagen: "camisa-de-boca.jpeg" },
+  { id: 20, nombre: "camisa de river", categoria: "deportes", stock: 10, precio: 3500, rutaimagen: "camisa-de-river.jpeg" },
+  { id: 21, nombre: "camisa de defensa", categoria: "deportes", stock: 10, precio: 3500, rutaimagen: "camisa-de-defensa.jpeg" },
+]
 
-const listar = (lista, propiedad1, propiedad2, propiedad3) =>
-  lista
-    .map(
-      (producto) =>
-        producto[propiedad1] +
-        " - " +
-        producto[propiedad2] +
-        " : " +
-        producto[propiedad3]
-    )
-    .join("\n");
 
-let inicio = prompt(
-  "bienvenido a Arclots desearia ver nuestros productos (si-no)\n2 - para filtrar nuestros productos por categoria"
-);
-if (inicio === "si") {
-  nuestrosProductos(productos);
-} else if (inicio === "2") {
-  filtrarProductos(productos);
-} else if (inicio === "no") {
-  alert("gracias por visitarnos");
+principal(productos)
+
+let carrito = []
+  
+
+function principal(productos) {
+
+  let botonInicio = document.getElementById("boton")
+  botonInicio.addEventListener("click", () => renderizarProductos(productos))
+  tarjetasdelosproductos(productos)
+}
+
+
+
+function renderizarProductos(productos) {
+  let productosFiltrados = filtrarProductos(productos)
+  tarjetasdelosproductos(productosFiltrados)
 }
 
 function filtrarProductos(productos) {
-  let categorias = [];
-  let inicio;
-
-  productos.forEach((producto) => {
-    if (!categorias.includes(producto.categoria)) {
-      categorias.push(producto.categoria);
-    }
-  });
-
-  let categoriaIngresada = prompt(
-    "ingresa una de las siguientes categorias: " +
-      categorias.join(", ") +
-      "\nsalir"
-  ).toLocaleLowerCase();
-  let categoriaFiltrada = productos.filter(
-    (producto) => producto.categoria === categoriaIngresada
-  );
-  let salida = categoriaFiltrada.map((producto) => producto.nombre).join("\n");
-  alert(salida);
+  let inputInicio = document.getElementById("input")
+  return productos.filter(producto => producto.nombre.includes(inputInicio.value))
 }
 
-function nuestrosProductos(productos) {
-  let carrito = [];
 
-  let productosVistos = productos.map((producto) => producto.nombre);
-  alert(productosVistos.join(" - "));
 
-  do {
-    opcion = Number(
-      prompt(
-        "Elija el producto que desea e ingrese su id para ver su informacion " +
-          " \n " +
-          listar(productos, "id", "nombre", "precio")
-      )
-    );
 
-    let porductoBuscado = productos.find((producto) => producto.id === opcion);
 
-    console.log(porductoBuscado);
 
-    carrito.push({
-      id: porductoBuscado.id,
-      nombre: porductoBuscado.nombre,
-      stock: porductoBuscado.stock,
-      precio: porductoBuscado.precio,
-    });
+function tarjetasdelosproductos(productos) {
+  
+ 
+  let nuestrosproductostarjeta = document.getElementById("nuestrosproductos")
 
-    console.log(carrito);
-  } while (opcion !== 0);
+  nuestrosproductostarjeta.innerHTML = ""
+
+  productos.forEach(productos => {
+
+      let { nombre, rutaimagen, precio, stock, id } = productos
+
+      let tarjetasproductos = document.createElement("div")
+      tarjetasproductos.className = "tarjetasproductos"
+      tarjetasproductos.innerHTML = `
+      <h3 class= "titulo" > ${nombre}</h3>
+      <img id="imagenes" class="imagenes2" src=./images/${rutaimagen} /> 
+      <h4 class="precio"> precio:${precio}</h4>
+      <p class="stock">stock: ${stock}</p>
+      <h4 class= "talles">talles: xs-s-m-l-x-xl-xxl</h4>
+      <button  id=${id} class="boton">agregar al carrito</button>
+      
+    `
+      nuestrosproductostarjeta.appendChild(tarjetasproductos)
+      let agregarAlcarrito = document.getElementById(productos.id)
+      agregarAlcarrito.addEventListener("click", agregarPrAlcarrito)
+
+  });
+
+}
+
+
+
+
+function agregarPrAlcarrito(e) {
+  
+  let idDeproducto = Number(e.target.id)
+  let productosEncarrito = carrito.findIndex(producto => producto.id === idDeproducto)
+  let productoBuscado = productos.find(producto => producto.id === idDeproducto)
+
+  if (productosEncarrito !== -1) {
+      carrito[productosEncarrito].unidades++
+      carrito[productosEncarrito].subtotal = carrito[productosEncarrito].precio * carrito[productosEncarrito].unidades
+  } else {
+      carrito.push({
+          id: productoBuscado.id,
+          nombre: productoBuscado.nombre,
+          unidades: 1,
+          precio: productoBuscado.precio,
+          subtotal: productoBuscado.precio
+
+      })
+  }
+  localStorage.setItem("carrito",JSON.stringify(carrito))
+  carritoProductos(carrito, productos)
+
+
+}
+
+function carritoProductos(carrito) {
+
+  
+  let losProductosCarrito = document.getElementById("productoscarrito")
+  losProductosCarrito.innerHTML = ""
+  carrito.forEach(productos => {
+      let tarjetaCarrito = document.createElement("div")
+      tarjetaCarrito.innerHTML = productos.nombre
+      losProductosCarrito.appendChild(tarjetaCarrito)
+      tarjetaCarrito.className = "tarjetacarrito"
+      tarjetaCarrito.innerHTML = `
+       
+       <p> ${productos.nombre}</p>
+       <p>${productos.precio}</p>
+       <p> ${productos.unidades}</p>
+       <p> ${productos.subtotal}</p>
+       <button id=eliminar${productos.id}>ELIMINAR</button>
+     
+      `
+  })
+}
+
+let botonCarrito = document.getElementById("carrito")
+
+botonCarrito.addEventListener("click", botonDeCarrito)
+
+function botonDeCarrito(e) {
+
+let carritoproductos = document.getElementById("nuestrosproductos")
+let productocarrito =  document.getElementById("productoscarrito")
+
+carritoproductos.classList.toggle("carrito")
+productocarrito.classList.toggle("carrito")
+
+ if (e.target.innerText === "carrito") {
+  e.target.innerText = "productos"
+  
+ }else(
+  e.target.innerText = "carrito"
+ )
+  
+
+}
+
+let botonOrdenar = document.getElementById("botonordenar")
+botonOrdenar.addEventListener("click", () => ordenar(productos))
+
+
+function ordenar(productos) {
+ productos.sort((a,b) => {
+  if (a.nombre > b.nombre) {
+      return 1
+  }else if (a.nombre < b.nombre){
+      return -1
+  }
+  return 0
+ })
+
+ tarjetasdelosproductos(productos)
+
+ 
 }
